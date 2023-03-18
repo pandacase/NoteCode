b7383a65fdcc8e3898706dee207c088ec1947c72
panda <2227119810@qq.com>
2023-03-18 17:04:06 +0800

    Create str_util.py

---
+++ python/my_utils/str_util.py
@@ -0,0 +1,11 @@
+def str_reverse(string):
+    return string[::-1]
+
+
+def substr(string, begin, end):
+    return string[begin:end]
+
+
+if __name__ == '__main__':
+    print("pandacase")
+    print("pandacase", 0, 5)
